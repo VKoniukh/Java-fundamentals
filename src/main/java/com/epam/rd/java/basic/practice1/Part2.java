f5d082634b1445ab2b8bf28aecc1d6941908e9a0
Volodymyr <konuhvladimir1@gmail.com>
2021-06-28 21:27:51 +0300

    commit Part2

--- src/main/java/com/epam/rd/java/basic/practice1/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part2.java
@@ -4,6 +4,13 @@ public class Part2 {
 
     public static void main(String[] args) {
 
+        result();
     }
-	
-}
+        static void result (int ...numbers){
+            int value = 0;
+            for(int n:numbers)
+                value += n;
+            System.out.print(value);
+        }
+
+    }
