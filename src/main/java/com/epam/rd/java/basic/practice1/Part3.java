25a28d30c036b3f2ac0a33525c242a6f7b801af5
Volodymyr <konuhvladimir1@gmail.com>
2021-06-28 22:58:07 +0300

    Part 3 (5)

--- src/main/java/com/epam/rd/java/basic/practice1/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part3.java
@@ -3,9 +3,10 @@ package com.epam.rd.java.basic.practice1;
 public class Part3 {
 
     public static void main(String[] args) {
+        StringBuilder strBuilder = new StringBuilder(args.length)
 
         for (int i = 0; i < args.length; i++){
-            System.out.print(args[i] + " ");
+            System.out.print(strBuilder.toString());
         }
 
     }
