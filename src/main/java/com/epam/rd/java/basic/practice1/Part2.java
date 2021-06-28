418dcd5db0a18c8aaee9b4dba48271d3b0f91e6e
Volodymyr <konuhvladimir1@gmail.com>
2021-06-28 22:05:00 +0300

    Part 2 (3)

--- src/main/java/com/epam/rd/java/basic/practice1/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part2.java
@@ -3,10 +3,10 @@ package com.epam.rd.java.basic.practice1;
 public class Part2 {
 
     public static void main(String[] args) {
-
         int num = 0;
-        for(String arg: args) {
+        for (String arg : args) {
             num += Integer.parseInt(arg);
         }
         System.out.print(num);
     }
+}
