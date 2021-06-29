366c7c27ae0f78f17cbced88acff67b4d2c3cb2c
Volodymyr <konuhvladimir1@gmail.com>
2021-06-29 12:57:22 +0300

    7

--- src/main/java/com/epam/rd/java/basic/practice1/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part3.java
@@ -3,8 +3,6 @@ package com.epam.rd.java.basic.practice1;
 public class Part3 {
 
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++){
-            System.out.print((args[i]+"").trim());
-        }
+            System.out.print(String.join(" ", args).trim());
     }
 }
