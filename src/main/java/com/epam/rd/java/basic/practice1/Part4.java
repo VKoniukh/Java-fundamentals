10edb8b3a1a0464c924f86511b2f35ea255c85f5
Volodymyr <konuhvladimir1@gmail.com>
2021-06-29 14:37:25 +0300

    1

--- src/main/java/com/epam/rd/java/basic/practice1/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part4.java
@@ -2,8 +2,23 @@ package com.epam.rd.java.basic.practice1;
 
 public class Part4 {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args1, args2) {
+        int a = args1
+        int b = args2
+        public int egcd(int a, int b) {
+            if (a == 0)
+                return b;
+
+            while (b != 0) {
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+
+            return a;
+        }
+
     }
 
 }
