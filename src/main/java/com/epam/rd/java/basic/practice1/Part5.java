1348b1703100a98561cb75014173e1f50e385b3b
Volodymyr <konuhvladimir1@gmail.com>
2021-06-30 14:53:01 +0300

    Part 5 [1]

--- src/main/java/com/epam/rd/java/basic/practice1/Part5.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part5.java
@@ -3,9 +3,14 @@ package com.epam.rd.java.basic.practice1;
 public class Part5 {
 
     public static void main(String[] args) {
+        String a = args [0];
+            String[] n = a.split("");
+            int m=1;
+            for(int i=1;i<n.length;i++){
+                m+=Integer.parseInt(n[0]);
+            }
+            System.out.print(m);
+        }
 
-//        получить цифры с ком строки
-        
-    }
-	
 }
+
