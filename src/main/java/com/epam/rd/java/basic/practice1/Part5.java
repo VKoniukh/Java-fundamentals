3094713e335d68624157c3c31eed74f81ee7a360
Volodymyr <konuhvladimir1@gmail.com>
2021-06-30 17:17:13 +0300

    Part 5 [2]

--- src/main/java/com/epam/rd/java/basic/practice1/Part5.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part5.java
@@ -3,14 +3,18 @@ package com.epam.rd.java.basic.practice1;
 public class Part5 {
 
     public static void main(String[] args) {
-        String a = args [0];
-            String[] n = a.split("");
-            int m=1;
-            for(int i=1;i<n.length;i++){
-                m+=Integer.parseInt(n[0]);
-            }
-            System.out.print(m);
+        String a = args[0];
+        String[] n = a.split("");
+        int size = n.length;
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = Integer.parseInt(n[i]);
         }
-
+        int sum = 0;
+        for (int j : arr) {
+            sum = sum + j;
+        }
+        System.out.println(sum);
+    }
 }
 
